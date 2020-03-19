985c7707f4a192b13c720d6dfd476b48c7edb508
Melenium2 <osumelenium@gmail.com>
2020-03-19 17:08:04 +0500

    semi-complete sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,92 +1,103 @@
 <template>
-  <div class="ei-sidebar ei-sidebar-fixed ei-sidebar-full" :class="{'ei-sidebar-mobile-active': triggered}">
-    <div class="ei-sidebar-header">
-      <div class="wrapper">
-        <img class="ei-sidebar-header-logo" :src="require('@/assets/images/Aiura_Logo.png')">
-        <i @click="$emit('trigger')">
-          <awesome-icon icon="caret-left" size="2x"></awesome-icon>
-        </i>
-      </div>
-    </div>
-    <div class="ei-sidebar-user">
-      <h5 class="ei-menu-text">Your balance</h5>
-      <div class="ei-sidebar-user-content">
-        <div class="balance">
-          <h1>20313 p.</h1>
-          <ei-button type="icon" icon="chart-line" size="st"></ei-button>
-        </div>
-        <div class="plan">
-          <span>Plan "Production plus"</span>
-          <ei-button type="icon" icon="external-link-alt" size="st"></ei-button>
-        </div>
-        <div class="plan">
-          <span>Last activity</span>
-          <span>3 days ago</span>
-        </div>
-        <div class="links">
-          <div class="buy-more">
-            <ei-button type="with-icon" icon="plus" size="st" flat>Buy more</ei-button>
+	<div class="ei-sidebar ei-sidebar-fixed ei-sidebar-full" :class="{'ei-sidebar-mobile-active': triggered}">
+		<div class="ei-sidebar-header">
+			<div class="wrapper">
+				<img class="ei-sidebar-header-logo" :src="require('@/assets/images/Aiura_Logo.png')" />
+				<i class="trigger" @click="$emit('trigger')">
+					<awesome-icon icon="caret-left" size="2x"></awesome-icon>
+				</i>
+			</div>
+		</div>
+      <simplebar class="ei-sidebar-body">
+        <div class="ei-sidebar-body-wrapper">
+          <div class="ei-sidebar-user">
+            <h5 class="ei-menu-text">Available balance</h5>
+            <div class="ei-sidebar-user-content">
+              <div class="balance">
+                <h1>20313 p.</h1>
+                <ei-button type="icon" icon="history" size="st"></ei-button>
+              </div>
+              <div class="plan">
+                <span>Plan:</span>
+                <span class="plan-name">"Some super"</span>
+              </div>
+              <div class="plan">
+                <span>Untill:</span>
+                <span class="plan-name">03/20/14</span>
+              </div>
+              <div class="links">
+                <div class="buy-more">
+                  <ei-button type="with-icon" icon="plus" size="st" flat>Add points</ei-button>
+                </div>
+                <div class="profile">
+                  <ei-button type="with-icon" icon="angle-double-up" size="st" standart>Upgrade</ei-button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="profile">
-            <ei-button type="with-icon" icon="user" size="st" standart>Profile</ei-button>
+          <h5 class="ei-menu-text">Menu</h5>
+          <div class="ei-sidebar-content">
+            <ul class="menu">
+              <li v-for="(item, id) in menu" :key="id" class="menu-item">
+                <a href :class="{ 'item-active': item.active }">
+                  <span class="icon">
+                    <awesome-icon :icon="item.icon"></awesome-icon>
+                  </span>
+                  <span>{{ item.name }}</span>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="ei-sidebar-content">
-      <h5 class="ei-menu-text">Menu</h5>
-      <ul class="menu">
-        <li v-for="(item, id) in menu" :key="id" class="menu-item">
-            <a href="" :class="{ 'item-active': item.active }">
-              <span class="icon"><awesome-icon :icon="item.icon"></awesome-icon></span>
-              <span>{{ item.name }}</span>
-            </a>
-        </li>
-      </ul>
-    </div>
-  </div>
+      </simplebar>
+	</div>
 </template>
 
 <script>
+import simplebar from 'simplebar-vue'
+import 'simplebar/dist/simplebar.min.css'
 
 export default {
-  props: {
-    triggered: {
-      type: Boolean,
-      default: false
-    }
+  components: {
+    simplebar
   },
-  data: () => ({
-    menu: [
-      {
-        name: 'Dashboard',
-        path: '/dashboard',
-        icon: 'chart-pie',
-        active: true
-      },
-      {
-        name: 'Api usage',
-        path: '/usage',
-        icon: 'network-wired',
-        active: false
-      },
-      {
-        name: 'Documentation',
-        path: '/docs',
-        icon: 'coins',
-        active: false
-      },
-      {
-        name: 'Support',
-        path: '/support',
-        icon: 'paper-plane',
-        active: false
-      }
-    ]
-  })
+	props: {
+		triggered: {
+			type: Boolean,
+			default: false
+		}
+	},
+	data: () => ({
+		menu: [
+			{
+				name: 'Dashboard',
+				path: '/dashboard',
+				icon: 'chart-pie',
+				active: true
+			},
+			{
+				name: 'Api usage',
+				path: '/usage',
+				icon: 'network-wired',
+				active: false
+			},
+			{
+				name: 'Documentation',
+				path: '/docs',
+				icon: 'coins',
+				active: false
+			},
+			{
+				name: 'Support',
+				path: '/support',
+				icon: 'paper-plane',
+				active: false
+			}
+		]
+	})
 }
 </script>
 
 <style lang="scss">
-@import "@/assets/scss/components/sidebar.scss";
+@import '@/assets/scss/components/sidebar.scss';
 </style>
--- src/config/_icons.js
+++ src/config/_icons.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faBars, faCaretLeft, faChartPie, faCoins,
-faNetworkWired, faPaperPlane, faChartLine, faPlus,
-faExternalLinkAlt, faUser } from '@fortawesome/free-solid-svg-icons'
+faNetworkWired, faPaperPlane, faHistory, faPlus,
+faExternalLinkAlt, faUser, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faBars, faCaretLeft, faChartPie, faCoins, faNetworkWired,
-  faPaperPlane, faChartLine, faPlus, faExternalLinkAlt, faUser)
+  faPaperPlane, faHistory, faPlus, faExternalLinkAlt, faUser,
+  faAngleDoubleUp)
 
 Vue.component('awesome-icon', FontAwesomeIcon)
